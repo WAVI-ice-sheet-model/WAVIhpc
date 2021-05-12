a90038f8abef094e332f2e5c5742b9a1594ea5da
not populated <aleey@bas.ac.uk>
2021-05-12 13:35:01 +0100

    added wavi install scripts

---
+++ scripts/julia_install_wavi.jl
@@ -0,0 +1,5 @@
+#Script to install WAVI. Choose the branch you'd like to install using the rev argument 
+
+using Pkg
+#Pkg.add("Example")
+Pkg.add("PackageSpec(url="https://github.com/RJArthern/WAVI.jl.git", rev="AlexDev_coupling"))
