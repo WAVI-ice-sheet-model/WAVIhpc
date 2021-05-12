f37f25e4c2c68bfb9fb59cf0094a070b6cd5f474
not populated <aleey@bas.ac.uk>
2021-05-12 12:51:10 +0100

    added pull_container and wavi_archer2 scripts

---
+++ scripts/wavi_archer2.sh
@@ -0,0 +1,26 @@
+# WAVI on ARCHER2 configuration script. Created by Alex Bradley (aleey@bas.ac.uk) on 12/5/21
+#!/bin/bash
+
+#
+# set environment variables
+#
+
+export W_ROOT=$WORK/wavi/WAVIhpc
+export TMPDIR=$WORK/SCRATCH
+export JDEPOT=$W_ROOT/.julia
+export IMGPATH=$W_ROOT/containers/julia_container_v1_6.img
+export HECACC=n02-bas
+export HOMEUSER="`whoami`"
+export HOMEHOST=$HOMEUSER@bslcenb.nerc-bas.ac.uk   # alias to 'null' to disable rsync
+export HOMEROOT="/data/oceans_output/shelf/`whoami`/WAVI"
+
+#
+# overwrite things with individual user exceptions
+#
+
+if [ `whoami` == "aleey" ] ; then
+  #export ROOTDIR=$JDEPOT/.julia_c2
+  export HECACC=n02-NES010475   # ENSOWAIS
+fi
+
+
