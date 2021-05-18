b400f7044e4c81ef6953487ec56c90f3509920f4
not populated <aleey@bas.ac.uk>
2021-05-18 10:03:28 +0100

    added interactive install script

---
+++ scripts/install_wavi_interactive.sh
@@ -0,0 +1,18 @@
+# Script to install wavi 
+
+#
+# choose install location (!! This will overwrite anything you have in this depot, be careful !!)
+#
+INSTALL_LOC=$W_ROOT/.julia
+rm -rf $INSTALL_LOC
+mkdir $INSTALL_LOC
+
+#
+# Go to scratch location and execute
+#
+cp julia_install_wavi.jl $WORK/julia_install_wavi.jl
+cd $WORK
+export SINGULARITYENV_JULIA_DEPOT_PATH="/opt/julia"
+singularity exec -B $INSTALL_LOC:/opt/julia $IMGPATH julia 
+rm julia_install_wavi.jl
+cd $W_ROOT/scripts/
