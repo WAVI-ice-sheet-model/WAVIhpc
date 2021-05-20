3f55322820aa806ff7b0e2fc1f11999abcbb0cb0
not populated <aleey@bas.ac.uk>
2021-05-20 10:04:30 +0100

    added copy case script

---
+++ scripts/copy_wavi_case_a2.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+# make a new case by copying an old case on ARCHER2
+# (without copying results files or unnecessary binaries)
+#
+# e.g. copy_wavi_case_a2.sh MISMIP_032 MISMIP_033
+
+WC=$W_ROOT/cases
+
+mkdir $WC/$2
+mkdir $WC/$2/run
+
+cp -ri $WC/$1/code $WC/$2
+cp -ri $WC/$1/input $WC/$2
+cp -ri $WC/$1/scripts $WC/$2
+
+# Changes the JOBNO in sub*.sh scripts if the two JOBIDS
+# contain an '_'
+cd $WC/$2/scripts
+
+var1u=`echo $1 | grep -b -o '_' | awk 'BEGIN {FS=":"}{print $1}'`
+var1u=`expr $var1u+1`
+
+var2u=`echo $2 | grep -b -o '_' | awk 'BEGIN {FS=":"}{print $1}'`
+var2u=`expr $var2u+1`
+
+IDstring="JOBNO="
+
+var1old=$IDstring${1:$var1u}
+var2new=$IDstring${2:$var2u}
+
+perl -pi -e "s/$var1old/$var2new/g" sub*.sh
+perl -pi -e "s/$var1old/$var2new/g" run_repeat.sh
+perl -pi -e "s/$var1old/$var2new/g" rsync*.sh
+
