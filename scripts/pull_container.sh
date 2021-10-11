fe078a1e4b3068d4a97c59189681fbe7adc50d9c
not populated <aleey@bas.ac.uk>
2021-10-11 10:55:29 +0100

    add pull container script

--- scripts/pull_container.sh
+++ scripts/pull_container.sh
@@ -1,5 +1,5 @@
 #Script to pull the julia container. Created by Alex Bradley (aleey@bas.ac.uk) on 12/05/2021
 
 IMAGE_NAME=julia_container_v1_6.img
-singularity pull --name $IMAGE_NAME docker://julia:1.6
+singularity pull --name $IMAGE_NAME docker://julia:1.6.1
 mv $IMAGE_NAME $W_ROOT/containers/.
