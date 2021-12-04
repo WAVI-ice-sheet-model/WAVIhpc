b67103aa1908cc4fb8dc46c0e99db7ef0723557a
James Byrne <jambyr@bas.ac.uk>
2021-12-04 17:01:50 +0000

    Initial commit of adapted functionality for local and BAS HPC

--- scripts/julia_install_wavi.jl
+++ scripts/julia_install_wavi.jl
@@ -1,8 +1,13 @@
-#Script to install WAVI. Choose the branch you'd like to install using the rev argument 
+#Script to install WAVI. Choose the branch you'd like to install using the rev argument
 
 using Pkg
-#Pkg.add("Example")
-#Pkg.add(url="https://github.com/RJArthern/WAVI.jl#AlexDev")
-Pkg.add(PackageSpec(url="https://github.com/RJArthern/WAVI.jl.git", rev="AlexDev"))
 
+env_name = "WAVI_ENV" in keys(ENV) ? ENV["WAVI_ENV"] : "wavi_test"
+Pkg.activate(env_name)
 
+repo = "WAVI_REPO" in keys(ENV) ? ENV["WAVI_REPO"] :
+    "https://github.com/RJArthern/WAVI.jl.git"
+rev = "WAVI_REV" in keys(ENV) ? ENV["WAVI_REV"] : "master"
+
+println("Using $repo and $rev")
+Pkg.add(PackageSpec(url=repo, rev=rev))
