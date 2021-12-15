0e904fb9e36bcba61737c09e8eef8edeb4f28d29
James Byrne <jambyr@bas.ac.uk>
2021-12-15 12:52:40 +0000

    Updated to deal with origin https

--- scripts/common.sh
+++ scripts/common.sh
@@ -8,7 +8,7 @@ if ! which julia; then
 fi
 
 WAVI_ROOT=`pwd`
-REPO_NAME=`git config -l | grep remote.origin.url | sed -r s'#^.+/([^/]+).git$#\1#'`
+REPO_NAME=`git config -l | grep remote.origin.url | sed -r s'#^.+/([^/]+)(.git)?$#\1#'`
 
 if [ "$REPO_NAME" != "WAVIhpc" ] || [ ! -d "$WAVI_ROOT/.git" ]; then
   echo "We don't appear to be running in the root of the WAVIhpc repo, which is required"
