c5302c683e39dd21a5a89c26a299fdade06f377d
James Byrne <jambyr@bas.ac.uk>
2022-01-07 10:55:33 +0000

    Update template_bas for SLURM enabled job resubmission

--- scripts/BAS/pre_run.sh
+++ scripts/BAS/pre_run.sh
@@ -1,3 +1,34 @@
 #!/usr/bin/env bash
 
 module load hpc/julia/1.6.2
+
+# https://unix.stackexchange.com/questions/146756/forward-sigterm-to-child-in-bash/444676#444676
+prep_term()
+{
+    unset term_child_pid
+    unset term_kill_needed
+    trap 'handle_term' TERM INT
+}
+
+handle_term()
+{
+    if [ "${term_child_pid}" ]; then
+        echo "Killing child process at `date +%F-%T`"
+        kill -TERM "${term_child_pid}" 2>/dev/null
+
+        echo -n "124" >LAST_EXIT
+    else
+        term_kill_needed="yes"
+    fi
+}
+
+wait_term()
+{
+    term_child_pid=$!
+    if [ "${term_kill_needed}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null 
+    fi
+    wait ${term_child_pid} 2>/dev/null
+    trap - TERM INT
+    wait ${term_child_pid} 2>/dev/null
+}
