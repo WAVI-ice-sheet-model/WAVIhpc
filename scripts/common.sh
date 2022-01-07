5ae31e5137f5f116686fcbdf5b7b4c3fc73fbb4f
James Byrne <jambyr@bas.ac.uk>
2022-01-07 15:09:28 +0000

    Remove timer from run_ensemble_member and better run dir check

--- scripts/common.sh
+++ scripts/common.sh
@@ -8,9 +8,8 @@ if ! which julia; then
 fi
 
 WAVI_ROOT=`pwd`
-REPO_NAME=`git config -l | grep remote.origin.url | sed -r s'#^.+/([^/]+)(.git)?$#\1#'`
 
-if [ "$REPO_NAME" != "WAVIhpc" ] || [ ! -d "$WAVI_ROOT/.git" ]; then
+if ! head -n 1 README.md | grep -q WAVIhpc || [ ! -d "$WAVI_ROOT/.git" ]; then
   echo "We don't appear to be running in the root of the WAVIhpc repo, which is required"
   exit 1
 fi
